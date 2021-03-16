3f61fb88cfb3f4f1f92bfe73b3a66b2322d204f0
Emanoel-Alves <emanoel-alves09@hotmail.com>
2021-03-16 00:06:43 -0300

    #1 Adicionado a tela de Artista com todas as funcionalidades

--- src/pages/Main/Home/Artist/index.tsx
+++ src/pages/Main/Home/Artist/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  inputSearch: {
+    height: 40,
+    borderColor: '#fff',
+    borderWidth: 1
+  },
+
+});
 
 const Artist = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <View>
-        <Text> Artist Page</Text>
+        <TextInput
+        style={styles.inputSearch}
+        defaultValue="You can type in me"
+      />
       </View>
     </View>
   );

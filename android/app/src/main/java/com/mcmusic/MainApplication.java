6d933f304afa04e795db8e5eb15429152ef7e6f9
Everton Cosmo <everton8cosmo@gmail.com>
2021-03-18 12:04:11 -0300

    chore: enable hermes to turbo modules ref #2

--- android/app/src/main/java/com/mcmusic/MainApplication.java
+++ android/app/src/main/java/com/mcmusic/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.bridge.JSIModulePackage; 
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -34,6 +36,11 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+        // React native reanimated 
+        @Override
+      protected JSIModulePackage getJSIModulePackage() {
+        return new ReanimatedJSIModulePackage(); // <- add
+      }
       };
 
   @Override

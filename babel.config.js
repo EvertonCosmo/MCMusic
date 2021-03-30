6b98cb08d5dd4971c291020a9b73eeed83f69c10
Everton Cosmo <everton8cosmo@gmail.com>
2021-03-30 20:41:23 -0300

    chore: redux thunk and new alias (utils) ref #2

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = {
         alias: {
           '@app/components': './src/components/index.ts',
           '@app/hooks': './src/context',
-          '@app/utils': './src/utils/index.ts',
+          '@app/utils': './src/utils',
         },
       },
     ],
